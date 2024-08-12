2f07e49327b611b7c3eea398845a00815374bcfb
Saim Cyber <saimzaib69@gmail.com>
2024-08-12 02:20:28 -0700

    ENCRYTION DONE

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,3 +1,24 @@
-what_want=input("type 'encrypt' for encryption and type 'decrypt' for decryption:\n")
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+
+def encryption(plain_text,shift_key):
+    cipher_text=""
+    for char in plain_text:
+        position=alphabet.index(char)
+        new_position=position+shift_key
+        cipher_text +=alphabet[new_position]
+    print(f"Here is the text after encryption: {cipher_text}")
+
+
+
+
+
+
+
+
+what_to_do=input("type 'encrypt' for encryption and type 'decrypt' for decryption:\n")
 text=input("Type your message:\n")
-shiftkey=int(input("Enter shift key:\n"))
+shift=int(input("Enter shift key:\n"))
+if what_to_do=="encrypt":
+    result=encryption(plain_text=text,shift_key=shift)
+#elif what_to_do=="decrypt":
+ #   decryption(ciphertext,shiftkey)
